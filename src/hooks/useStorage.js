ebf2006653d797f5e3a24dcac09b2672066abaac
g-priime <57049481+g-priime@users.noreply.github.com>
2021-02-06 15:30:25 -0700

    Create useStorage.js

---
+++ src/hooks/useStorage.js
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+import {
+  projectStorage,
+  projectFirestore,
+  timestamp,
+} from "../firebase";
+
+const useStorage = (file, location) => {
+  const [progress, setProgress] = useState(0);
+  const [error, setError] = useState(null);
+  const [url, setUrl] = useState(null);
+
+  useEffect(() => {
+    // references
+    const storageRef = projectStorage.ref(file.name);
+    const collectionRef = projectFirestore.collection("images");
+
+    const fileName = file.name;
+
+    storageRef.put(file).on(
+      "state_changed",
+      (snap) => {
+        let percentage = (snap.bytesTransferred / snap.totalBytes) * 100;
+        setProgress(percentage);
+      },
+      (err) => {
+        setError(err);
+      },
+      async () => {
+        const url = await storageRef.getDownloadURL();
+        const createdAt = timestamp();
+        collectionRef.doc(fileName).set({ name: fileName, url, createdAt, location });
+        setUrl(url);
+
+        console.log(location);
+      }
+    );
+  }, [file, location]);
+
+  return { progress, url, error };
+};
+
+export default useStorage;
