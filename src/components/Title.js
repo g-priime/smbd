e45111f929d40cc48e895d685aea501d32bb435b
g-priime <57049481+g-priime@users.noreply.github.com>
2021-02-06 15:31:04 -0700

    Create Title.js

---
+++ src/components/Title.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Title = ({ setShowForm }) => {
+  const handleClick = () => {
+    setShowForm(true);
+  };
+
+  return (
+    <div className="title">
+      <header>
+        <h1>SMBD</h1>
+        <button onClick={handleClick}>Add Photo</button>
+      </header>
+      <h2>So Much Beauty in Dirt</h2>
+      {/*
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      */}
+    </div>
+  );
+};
+
+export default Title;
