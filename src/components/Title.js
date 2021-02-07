f095213c3cd52fb73c3ce9d527aa1c1da4a0b220
g-priime <57049481+g-priime@users.noreply.github.com>
2021-02-07 14:05:25 -0700

    Update Title.js

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,6 +1,13 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 const Title = ({ setShowForm }) => {
+  const history = useHistory();
+
+  const toProfile = () => {
+    history.push("/");
+  };
+
   const handleClick = () => {
     setShowForm(true);
   };
@@ -9,7 +16,7 @@ const Title = ({ setShowForm }) => {
     <div className="title">
       <header>
         <h1>SMBD</h1>
-        <button onClick={handleClick}>Profile</button>
+        <button onClick={toProfile}>Profile</button>
         <button onClick={handleClick}>Add Photo</button>
       </header>
       <h2>So Much Beauty in Dirt</h2>
