03a0ffdb637f108d07044e14ad8e25058c879530
g-priime <57049481+g-priime@users.noreply.github.com>
2021-02-07 12:31:39 -0700

    Update Title.js

--- src/components/Title.js
+++ src/components/Title.js
@@ -9,6 +9,7 @@ const Title = ({ setShowForm }) => {
     <div className="title">
       <header>
         <h1>SMBD</h1>
+        <button onClick={handleClick}>Profile</button>
         <button onClick={handleClick}>Add Photo</button>
       </header>
       <h2>So Much Beauty in Dirt</h2>
