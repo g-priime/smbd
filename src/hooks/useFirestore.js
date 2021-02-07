a75d2607ae83257bcf688dd11a127008f4142c14
g-priime <57049481+g-priime@users.noreply.github.com>
2021-02-07 16:04:55 -0700

    Update useFirestore.js

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -7,7 +7,6 @@ const useFirestore = (collection) => {
   const [docs, setDocs] = useState([]);
 
   const { currentUser } = useAuth();
-  console.log(currentUser.email);
 
   useEffect(() => {
     const unsub = projectFirestore
@@ -16,16 +15,12 @@ const useFirestore = (collection) => {
       .onSnapshot((snap) => {
         let documents = [];
         snap.forEach((doc) => {
-          documents.push({ ...doc.data(), id: doc.id });
+          if (doc.data().email === currentUser.email) {
+            documents.push({ ...doc.data(), id: doc.id });
+          }
         });
 
-        let userDocuments = [];
-        for (let x = 0; x < documents.length; x++) {
-          if (documents[x].email === currentUser.email) {
-            userDocuments.push(documents[x]);
-          }
-        }
-        setDocs(userDocuments);
+        setDocs(documents);
       });
 
     return () => unsub();
