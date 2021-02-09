5eb64deec8eda7089dd271c69e5f704853b1cc47
g-priime <57049481+g-priime@users.noreply.github.com>
2021-02-08 18:35:58 -0700

    Update Title.js

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,25 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
+import { Alert } from "react-bootstrap";
 
 const Title = ({ setShowForm }) => {
   const history = useHistory();
+  const [error, setError] = useState("");
+  const { logout, currentUser } = useAuth();
 
-  const toProfile = () => {
-    history.push("/");
+  const toUpdateProfile = () => {
+    history.push("/update-profile");
   };
 
   const handleClick = () => {
     setShowForm(true);
   };
 
+  async function handleLogout() {
+    setError("");
+
+    try {
+      await logout();
+      history.push("/login");
+    } catch {
+      setError("Failed to log out");
+    }
+  }
+
   return (
     <div className="title">
       <header>
         <h1>SMBD</h1>
-        <button onClick={toProfile}>Profile</button>
+        <h3>{currentUser.email}</h3>
+        
         <button onClick={handleClick}>Add Photo</button>
+        <button onClick={toUpdateProfile}>Update Profile</button>
+        <button onClick={handleLogout}>Log Out</button>
       </header>
+      
       <h2>So Much Beauty in Dirt</h2>
+      {/* TODO - fix how error message is shown */}
+      {error && <Alert variant="danger">{error}</Alert>}
       {/*
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
       */}
