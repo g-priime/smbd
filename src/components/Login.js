e355833529d611623abc56351db03389c31426bd
g-priime <57049481+g-priime@users.noreply.github.com>
2021-02-10 17:50:31 -0700

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -47,9 +47,9 @@ export default function Login() {
                 <Form.Label>Password</Form.Label>
                 <Form.Control type="password" ref={passwordRef} required />
               </Form.Group>
-              <Button disabled={loading} className="w-100" type="submit">
+              <button disabled={loading} className="w-100 button-forms" type="submit">
                 Log In
-              </Button>
+              </button>
             </Form>
             <div className="w-100 text-center mt-3">
               <Link to="/forgot-password">Forgot Password?</Link>
