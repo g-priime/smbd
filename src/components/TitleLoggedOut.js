8260039ddcc6da6f8a9a5c56aa69dc1a3eabfd2f
g-priime <57049481+g-priime@users.noreply.github.com>
2021-02-14 16:05:03 -0700

    Update TitleLoggedOut.js

--- src/components/TitleLoggedOut.js
+++ src/components/TitleLoggedOut.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useHistory, Link } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 import { Alert } from "react-bootstrap";
 
@@ -8,7 +8,28 @@ const TitleLoggedOut = ({ setShowForm }) => {
   const [error, setError] = useState("");
   const { logout } = useAuth();
 
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener("resize", showButton);
+
   async function handleLogin() {
+    closeMobileMenu();
     setError("");
 
     try {
@@ -21,12 +42,19 @@ const TitleLoggedOut = ({ setShowForm }) => {
 
   return (
     <div className="title">
-      <div className="header-logged-out">
-        <h1>SMBD</h1>
-
-        <button onClick={handleLogin}>Log In</button>
-      </div>
-
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+            <h1>SMBD</h1> <i className="fab fa-typo3" />
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <button onClick={handleLogin}>Log In</button>
+          </ul>
+        </div>
+      </nav>
       <h2>Create Your Account</h2>
       {/* TODO - fix how error message is shown */}
       {error && <Alert variant="danger">{error}</Alert>}
