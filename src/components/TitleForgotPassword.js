f2e8b85f9540009e81f3a09710b124892999f64b
g-priime <57049481+g-priime@users.noreply.github.com>
2021-02-14 16:05:06 -0700

    Update TitleForgotPassword.js

--- src/components/TitleForgotPassword.js
+++ src/components/TitleForgotPassword.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useHistory,Link } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 import { Alert } from "react-bootstrap";
 
@@ -8,7 +8,28 @@ const TitleForgotPassword = ({ setShowForm }) => {
   const [error, setError] = useState("");
   const { logout } = useAuth();
 
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener("resize", showButton);
+
   async function handleLogin() {
+    closeMobileMenu();
     setError("");
 
     try {
@@ -20,6 +41,7 @@ const TitleForgotPassword = ({ setShowForm }) => {
   }
 
   async function handleSignUp() {
+    closeMobileMenu();
     setError("");
 
     try {
@@ -32,13 +54,20 @@ const TitleForgotPassword = ({ setShowForm }) => {
 
   return (
     <div className="title">
-      <div className="header-forgot-password">
-        <h1>SMBD</h1>
-
-        <button onClick={handleLogin}>Log In</button>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+            <h1>SMBD</h1> <i className="fab fa-typo3" />
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <button onClick={handleLogin}>Log In</button>
         <button onClick={handleSignUp}>Sign Up</button>
-      </div>
-
+          </ul>
+        </div>
+      </nav>
       <h2>Reset Your Password</h2>
       {/* TODO - fix how error message is shown */}
       {error && <Alert variant="danger">{error}</Alert>}
