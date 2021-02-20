a13980bb02466aa8487b22c69d95f7c00cb98091
g-priime <57049481+g-priime@users.noreply.github.com>
2021-02-20 13:46:27 -0700

    Update MyGallery.js

--- src/components/MyGallery.js
+++ src/components/MyGallery.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
-import ImageGrid from "./ImageGrid";
+
 import Modal from "./Modal";
-import Title from "./Title";
+
 import UploadForm from "./UploadForm";
 
 import "../index.css"
 import TitleMyGallery from "./TitleMyGallery";
+import MyGrid from "./MyGrid"
 
 function MyGallery() {
   const [selectedImg, setSelectedImg] = useState(null);
@@ -19,7 +20,7 @@ function MyGallery() {
       <TitleMyGallery setShowForm={setShowForm} />
       {showForm && <UploadForm setShowForm={setShowForm} />} {/* comment out to remove upload ability */}
       
-      <ImageGrid setSelectedImg={setSelectedImg} />
+      <MyGrid setSelectedImg={setSelectedImg} />
       {selectedImg && (
         <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />
       )}
