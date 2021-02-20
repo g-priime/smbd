181a78f06e65ea0a8c5f330009f1b836411e79aa
g-priime <57049481+g-priime@users.noreply.github.com>
2021-02-20 13:18:39 -0700

    Create TitleMyGallery.js

---
+++ src/components/TitleMyGallery.js
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from "react";
+import { useHistory, Link } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
+import { Alert } from "react-bootstrap";
+
+import "./Navbar.css";
+
+const TitleMyGallery = ({ setShowForm }) => {
+  const history = useHistory();
+  const [error, setError] = useState("");
+  const { logout, currentUser } = useAuth();
+
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener("resize", showButton);
+
+  const toUpdateProfile = () => {
+    closeMobileMenu();
+    history.push("/update-profile");
+  };
+
+  const addPhoto = () => {
+    closeMobileMenu();
+    setShowForm(true);
+  };
+
+  async function handleLogout() {
+    closeMobileMenu();
+    setError("");
+
+    try {
+      await logout();
+      history.push("/login");
+    } catch {
+      setError("Failed to log out");
+    }
+  }
+
+  return (
+    <div className="title">
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+            <h1>SMBD</h1> <i className="fas fa-leaf" />
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <button onClick={addPhoto}>Add Photo</button>
+
+            <button onClick={toUpdateProfile}>Update Profile</button>
+
+            <button onClick={handleLogout}>Log Out</button>
+          </ul>
+        </div>
+      </nav>
+
+      <h2>So Much Beauty in Dirt</h2>
+      {/* TODO - fix how error message is shown */}
+      {error && <Alert variant="danger">{error}</Alert>}
+      {/*
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      */}
+    </div>
+  );
+};
+
+export default TitleMyGallery;
