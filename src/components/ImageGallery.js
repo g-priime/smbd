0319896d1bc771bca20d81ad60bd64f79654df88
g-priime <57049481+g-priime@users.noreply.github.com>
2021-02-21 15:52:47 -0700

    Update ImageGallery.js

--- src/components/ImageGallery.js
+++ src/components/ImageGallery.js
@@ -5,22 +5,23 @@ import Title from "./Title";
 import UploadForm from "./UploadForm";
 
 import "../index.css"
+import useAllPics from "../hooks/useAllPics"
 
 function ImageGallery() {
   const [selectedImg, setSelectedImg] = useState(null);
 
   const [showForm, setShowForm] = useState(false);
 
-  
+  const { docs } = useAllPics("images");
 
   return (
     <div className="ImageGallery">
       <Title setShowForm={setShowForm} />
       {showForm && <UploadForm setShowForm={setShowForm} />} {/* comment out to remove upload ability */}
       
-      <ImageGrid setSelectedImg={setSelectedImg} />
+      <ImageGrid setSelectedImg={setSelectedImg} docs={docs} />
       {selectedImg && (
-        <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />
+        <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} docs={docs} />
       )}
     </div>
   );
